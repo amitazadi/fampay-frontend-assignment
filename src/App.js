bd3075f3a92ee21597793f8d911f484d79319cf1
Amit Kumar <khurjaamit.com>
2022-07-15 22:35:10 +0530

    logo added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./index.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <div className="logo-heading">
+        <img className="logo" src={logo} alt="fampay-logo" />
+      </div>
+
+      <div className="cards">
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
